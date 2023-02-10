4a8e8b3888ff3e8c3a15e3954746361836a0e9d4
ousseinithegreathest <adnanejiko@gmail.com>
2023-02-10 01:35:08 +0000

    skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./sections/Hero";
+import { Skills } from "./sections/Skills";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Hero />
+      <Skills />
     </div>
   );
 }
