3ba8ce5ca447e832cdec2e72902c0020a92b1922
ousseinithegreathest <adnanejiko@gmail.com>
2023-02-12 03:16:16 +0000

    update skills section

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -1,59 +1,54 @@
 const skills = [
+  // frontend
   {
-    frontend: [
-      {
-        id: 'HTML',
-        img: './images/skills/html5.svg'
-      },
-      {
-        id: 'CSS',
-        img: './images/skills/css3.svg'
-      },
-      {
-        id: 'Javascript',
-        img: './images/skills/javascript.svg'
-      },
-      {
-        id: 'Bootstrap',
-        img: './images/skills/bootstrap.svg'
-      },
-      {
-        id: 'React JS',
-        img: './images/skills/react.svg'
-      },
-      {
-        id: 'Flutter',
-        img: './images/skills/flutter.svg'
-      },
-    ],
-    backend: [
-      {
-        id: 'PHP',
-        img: './images/skills/php.svg'
-      },
-      {
-        id: '.NET Core',
-        img: './images/skills/net-core.svg'
-      },
-      {
-        id: 'Django',
-        img: './images/skills/django.svg'
-      },
-    ],
-    languages: [
-      {
-        id: 'Dart',
-        img: './images/skills/dart.svg'
-      },
-      {
-        id: 'Python',
-        img: './images/skills/python.svg'
-      },
-      {
-        id: 'C#',
-        img: './images/skills/csharp.svg'
-      },
-    ],
+    id: 'HTML',
+    img: './images/skills/html5.svg'
+  },
+  {
+    id: 'CSS',
+    img: './images/skills/css3.svg'
+  },
+  {
+    id: 'Javascript',
+    img: './images/skills/javascript.svg'
+  },
+  {
+    id: 'Bootstrap',
+    img: './images/skills/bootstrap.svg'
+  },
+  {
+    id: 'React JS',
+    img: './images/skills/react.svg'
+  },
+  {
+    id: 'Flutter',
+    img: './images/skills/flutter.svg'
+  },
+  //backend 
+  {
+    id: 'PHP',
+    img: './images/skills/php.svg'
+  },
+  {
+    id: '.NET Core',
+    img: './images/skills/net-core.svg'
+  },
+  // languages
+  {
+    id: 'Java',
+    img: './images/skills/java.svg'
+  },
+  {
+    id: 'Dart',
+    img: './images/skills/dart.svg'
+  },
+  {
+    id: 'C#',
+    img: './images/skills/csharp.svg'
+  },
+  {
+    id: 'Python',
+    img: './images/skills/python.svg'
   },
 ];
 
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -3,37 +3,19 @@ import SkillsData from "../data/Skills"
 import '../styles/Skills.css'
 
 export const Skills = () => {
-    const [frontendSkills, setFrontendSkills] = useState([])
-    const [backendSkills, setBackendSkills] = useState([])
-    const [languageSkills, setLanguageSkills] = useState([])
+    const [skills, setSkills] = useState([])
+
 
     useEffect(() => {
-        let frontend = [];
-        SkillsData.forEach((skill) => {
-            frontend = [...frontend, ...skill.frontend];
-        });
-        setFrontendSkills(frontend)
-
-        let backend = []
-        SkillsData.forEach((skill) => {
-            backend = [...backend, ...skill.backend];
-        });
-        setBackendSkills(backend)
-
-        let languages = []
-        SkillsData.forEach((skill) => {
-            languages = [...languages, ...skill.languages];
-        });
-        setLanguageSkills(languages)
+        setSkills(SkillsData)
     }, []);
     return (
         <div className='skills-container'>
             <h2>My skills</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta vitae vero qui iure quidem sit itaque consectetur libero blanditiis nulla earum voluptas quaerat nostrum, aspernatur sapiente impedit ratione quia ducimus enim placeat consequuntur est, eos ut. Cumque, quasi id dolor ut nam fugit dicta cupiditate, deserunt voluptatem facere doloremque ipsam deleniti quos mollitia. Eveniet tenetur debitis ad, alias cupiditate nostrum voluptatibus quas quos perferendis. Provident corporis autem quam odio esse?</p>
-            <h3>Front-end</h3>
             <ul className="items">
                 {
-                    frontendSkills.map((item) => {
+                    skills.map((item) => {
                         return (
 
                             <li key={item.id}><img src={item.img} alt={item.id} /></li>
@@ -42,31 +24,7 @@ export const Skills = () => {
                     })
                 }
             </ul>
-            <h3>Back-end</h3>
-            <ul className="items">
-                {
-                    backendSkills.map((item) => {
-                        return (
-
-                            <li key={item.id}><img src={item.img} alt={item.id} /></li>
-
-                        );
-                    })
-                }
-            </ul>
-            <h3>Languages</h3>
-            <ul className="items">
-                {
-                    languageSkills.map((item) => {
-                        return (
-
-                            <li key={item.id}><img src={item.img} alt={item.id} /></li>
-
-                        );
-                    })
-                }
-            </ul>
-
         </div>
+
     )
 }
